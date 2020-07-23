0957f0dbe290718340a60de3556168d8b8200b59
Tom White <tomwhite007@gmail.com>
2020-07-23 08:31:08 +0100

    generate and import dummy module

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { DummyModule } from './dummy/dummy.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    DummyModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
